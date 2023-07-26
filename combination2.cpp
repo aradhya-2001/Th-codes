9ac50d62e43a209242303019fa851c18cfb3872e
aradhya <singh.aradhya2019@gmail.com>
2023-07-27 01:05:56 +0530

    7

--- combination2.cpp
+++ combination2.cpp
@@ -15,7 +15,7 @@ void comb(int curr, int prev, int* boxes, int& r, int& n) // prev is last select
     for(int i = prev + 1; i < n; i++)
     {
         boxes[i] = curr; // as func is being called on bigger no i.e. curr+1, this bigger num is always placed after the prev num i.e. on prev+1
-        comb(curr + 1, i, boxes, r, n); // since num is already placed at i sou for next func, prev is i.
+        comb(curr + 1, i, boxes, r, n); // since num is already placed at i so for next func, prev is i.
         boxes[i] = 0;
     }
 }
